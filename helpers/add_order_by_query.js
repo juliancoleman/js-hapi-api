42a8512708e660fb393fa4c9de6508d51a99d6fa
Julian <julcol03@gmail.com>
2017-09-13 12:35:49 -0700

    lint

--- helpers/add_order_by_query.js
+++ helpers/add_order_by_query.js
@@ -4,7 +4,7 @@ const { knex } = appRequire("config/bookshelf");
 
 module.exports = (qb, sort) => {
   const isDescending = sort.startsWith("-");
-  const orderByColumn = isDescending ? R.takeLast(sort.length - 1, sort): sort;
+  const orderByColumn = isDescending ? R.takeLast(sort.length - 1, sort) : sort;
   const orderByDirection = isDescending ? "desc" : "asc";
 
   qb.orderBy(knex.raw(`LOWER(${orderByColumn})`), orderByDirection);
